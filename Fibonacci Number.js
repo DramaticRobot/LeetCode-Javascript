d6e0b969ad654b0365fab2aa8836fd9f34aee4c0
David Crary <davidc@epiloglaser.com>
2023-01-12 12:49:41 -0500

    Works but slow

---
+++ Fibonacci Number.js
@@ -0,0 +1,47 @@
+// Problem: https://leetcode.com/explore/learn/card/recursion-i/255/recursion-memoization/1661/
+
+// Fibonacci sequence, such that each number is the sum of the two preceding ones
+// Use recursion and memoization hash table
+
+const startTime = performance.now();
+
+n = 4;
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+var fib = function(n) {
+    // F(n) = F(n - 1) + F(n - 2)
+    // F(0) = 0, F(1) = 1
+
+    const storedN = new Map();
+    return fibonacci(n);
+
+    function fibonacci(n) {
+        let result = 0;
+        if (n < 2)
+            return n;
+        else {
+            let precedingOne = (n - 1);
+            if (storedN.has(precedingOne))
+                precedingOne = storedN.get(precedingOne);
+        
+            let precedingTwo = (n - 2);
+            if (storedN.has(precedingTwo))
+                precedingTwo = storedN.get(precedingTwo);
+        
+            result = fib(precedingOne) + fib(precedingTwo);
+        }
+    
+        storedN.set(n, result);
+        return result;
+    }
+};
+
+let ans = fib(n);
+
+const endTime = performance.now();
+const elapsedTime = endTime - startTime;
+console.log(ans); console.log(elapsedTime.toFixed(4) + ' Milliseconds');
